14c3d60598762b360fcd7ab404703866f134933b
Benjamín <brj46@hi.is>
2025-03-20 20:35:11 +0000

    hæ Tommi

--- src/app/flokkar/[flokkur]/page.tsx
+++ src/app/flokkar/[flokkur]/page.tsx
@@ -1,12 +1,14 @@
 import { Category } from '@/components/Category/Category';
 import Navigation from '@/components/Navigation/Navigation';
 
-export default async function FlokkaPage({
-  params,
-}: {
-  params: Promise<{ flokkur: string }>;
-}) {
-  const { flokkur } = await params;
+interface FlokkurPageProps {
+  params: {
+    flokkur: string;
+  };
+}
+
+export default function FlokkurPage({ params }: FlokkurPageProps) {
+  const { flokkur } = params;
 
   return (
     <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <div className={styles.page}>
       <Navigation />
       forsíða!
+      hér er eithvað texti
     </div>
   );
 }
