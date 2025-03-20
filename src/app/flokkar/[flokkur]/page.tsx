62a2464dd71239176dd4f6957740a08dfb643c75
Benjamín <brj46@hi.is>
2025-03-20 23:17:01 +0000

    404 in slug

--- src/app/flokkar/[flokkur]/page.tsx
+++ src/app/flokkar/[flokkur]/page.tsx
@@ -1,5 +1,7 @@
 import { Category } from '@/components/Category/Category';
 import Navigation from '@/components/Navigation/Navigation';
+import { notFound } from 'next/navigation';
+import { QuestionsApi } from '@/api';
 
 export default async function FlokkaPage({
   params,
@@ -7,6 +9,13 @@ export default async function FlokkaPage({
   params: Promise<{ flokkur: string }>;
 }) {
   const { flokkur } = await params;
+
+  const api = new QuestionsApi();
+  const category = await api.getCategory(flokkur);
+
+  if (!category) {
+    notFound();
+  }
 
   return (
     <div>
