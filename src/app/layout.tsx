569618e0cdf59d2b87d177ca6c0c6ebe19617f12
Benjamín <brj46@hi.is>
2025-03-20 23:41:31 +0000

    verkefni 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Halló heimur",
+  title: "Verkefni 4",
 };
 
 export default function RootLayout({
