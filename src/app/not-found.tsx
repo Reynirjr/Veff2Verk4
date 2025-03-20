6d482ddc590a9161830195ed7b8c8e68e7a7cb31
Benjamín <brj46@hi.is>
2025-03-20 23:13:28 +0000

    404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import styles from './not-found.module.css';
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h1>404 - Not Found</h1>
+      {}
+    </div>
+  );
+}
